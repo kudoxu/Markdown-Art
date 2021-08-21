7347aefed5956bfed6b30d4708cf03518660b4e6
kudoxu <a1152247078@hotmail.com>
2021-08-21 09:44:40 +0800

    sync

---
+++ Mind Map/画图笔记本电脑推荐.mm
@@ -0,0 +1,33 @@
+
+<map>
+  <node ID="root" TEXT="画图笔记本电脑推荐">
+    <node TEXT="" ID="qXkn7Ylzi1" _mubu_text="" STYLE="bubble" POSITION="default"/>
+    <node TEXT="轻薄小巧性能好" ID="VAzGbPRI7r" _mubu_text="%3Cspan%3E%E8%BD%BB%E8%96%84%E5%B0%8F%E5%B7%A7%E6%80%A7%E8%83%BD%E5%A5%BD%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="3D建模画图" ID="wRYzaDJlFP" _mubu_text="%3Cspan%3E3D%E5%BB%BA%E6%A8%A1%E7%94%BB%E5%9B%BE%3C/span%3E" STYLE="fork">
+        <node TEXT="联想小新Air 14 2021" ID="vpvfs5XK2w" _mubu_text="%3Cspan%3E%E8%81%94%E6%83%B3%E5%B0%8F%E6%96%B0Air%2014%202021%3C/span%3E" STYLE="fork"/>
+        <node TEXT="联想小新Air 14 Plus" ID="EVotMPPI6K" _mubu_text="%3Cspan%3E%E8%81%94%E6%83%B3%E5%B0%8F%E6%96%B0Air%2014%20Plus%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="平面设计" ID="LvG9K0LImZ" _mubu_text="%3Cspan%3E%E5%B9%B3%E9%9D%A2%E8%AE%BE%E8%AE%A1%3C/span%3E" STYLE="fork">
+        <node TEXT="无触屏需求" ID="nuKsUl9nWY" _mubu_text="%3Cspan%3E%E6%97%A0%E8%A7%A6%E5%B1%8F%E9%9C%80%E6%B1%82%3C/span%3E" STYLE="fork">
+          <node TEXT="联想小新Pro 14标压锐龙版" ID="Z8z1Yt8BRn" _mubu_text="%3Cspan%3E%E8%81%94%E6%83%B3%E5%B0%8F%E6%96%B0Pro%2014%E6%A0%87%E5%8E%8B%E9%94%90%E9%BE%99%E7%89%88%3C/span%3E" STYLE="fork"/>
+          <node TEXT="联想YOGA 14s 标压新锐龙版" ID="ElYz97Y1mO" _mubu_text="%3Cspan%3E%E8%81%94%E6%83%B3YOGA%2014s%20%E6%A0%87%E5%8E%8B%E6%96%B0%E9%94%90%E9%BE%99%E7%89%88%3C/span%3E" STYLE="fork"/>
+          <node TEXT="联想YOGA Pro 13s Carbon 2021酷睿版" ID="mVsRbaqc0i" _mubu_text="%3Cspan%3E%E8%81%94%E6%83%B3YOGA%20Pro%2013s%20Carbon%202021%E9%85%B7%E7%9D%BF%E7%89%88%3C/span%3E" STYLE="fork"/>
+          <node TEXT="红米Pro 14" ID="NwBkLeolrq" _mubu_text="%3Cspan%3E%E7%BA%A2%E7%B1%B3Pro%2014%3C/span%3E" STYLE="fork"/>
+        </node>
+        <node TEXT="有触屏需求" ID="yBrbUfGwrU" _mubu_text="%3Cspan%20class=%22strikethrough%22%3E%E6%9C%89%E8%A7%A6%E5%B1%8F%E9%9C%80%E6%B1%82%3C/span%3E" STYLE="fork">
+          <node TEXT="戴尔XPS13-9310" ID="ilWU78m4dI" _mubu_text="%3Cspan%20class=%22strikethrough%22%3E%E6%88%B4%E5%B0%94XPS13-9310%3C/span%3E" STYLE="fork"/>
+        </node>
+      </node>
+    </node>
+    <node TEXT="稍重稍大性能好" ID="plP5ZnlmdI" _mubu_text="%3Cspan%3E%E7%A8%8D%E9%87%8D%E7%A8%8D%E5%A4%A7%E6%80%A7%E8%83%BD%E5%A5%BD%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="3D建模画图" ID="47ZRH8zBOo" _mubu_text="%3Cspan%3E3D%E5%BB%BA%E6%A8%A1%E7%94%BB%E5%9B%BE%3C/span%3E" STYLE="fork">
+        <node TEXT="联想R7000 2021" ID="Vtw9ugdS4Y" _mubu_text="%3Cspan%3E%E8%81%94%E6%83%B3R7000%202021%3C/span%3E" STYLE="fork"/>
+        <node TEXT="联想小新Pro 16" ID="dpJLx07Wtc" _mubu_text="%3Cspan%3E%E8%81%94%E6%83%B3%E5%B0%8F%E6%96%B0Pro%2016%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="平面设计" ID="l8f7EIZKnY" _mubu_text="%3Cspan%3E%E5%B9%B3%E9%9D%A2%E8%AE%BE%E8%AE%A1%3C/span%3E" STYLE="fork">
+        <node TEXT="有触屏需求" ID="GVavMFGWZB" _mubu_text="%3Cspan%3E%E6%9C%89%E8%A7%A6%E5%B1%8F%E9%9C%80%E6%B1%82%3C/span%3E" STYLE="fork"/>
+        <node TEXT="无触屏需求" ID="pUXusPa3pA" _mubu_text="%3Cspan%3E%E6%97%A0%E8%A7%A6%E5%B1%8F%E9%9C%80%E6%B1%82%3C/span%3E" STYLE="fork"/>
+      </node>
+    </node>
+  </node>
+</map>
